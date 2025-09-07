86777542133634a83e12fa8b9436e7b1f60eee2d
Menna Ashraf <mennaalla1512@gmail.com>
2025-09-07 19:07:49 +0300

    update localStorage

--- src/app/feature/auth/services/auth.service.ts
+++ src/app/feature/auth/services/auth.service.ts
@@ -7,16 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
   constructor(private readonly _HttpClient: HttpClient) {
-    if (localStorage.getItem('userToken') !== null) this.getProfile();
   }
   public email: string | any = '';
-  getProfile() {
-    let encoded: any = localStorage.getItem('userToken');
-    let decoded: any = jwtDecode(encoded);
-    console.log(decoded);
-    localStorage.setItem('role', decoded.role);
-    localStorage.setItem('userName', decoded.userName);
-  }
+
 
   login(data: any): Observable<any> {
     return this._HttpClient.post(`admin/users/login`, data);
