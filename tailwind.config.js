23ab0ddb68617269edf3d64d10b9b01653b23284
Menna Ashraf <mennaalla1512@gmail.com>
2025-09-07 23:16:51 +0300

    facilitiesList

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
   important: true,
-}
-
+};
