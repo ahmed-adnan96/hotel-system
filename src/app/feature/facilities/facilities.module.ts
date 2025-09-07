8f2d0bfdada0994f8616cfb3b9559d240d779f43
Menna Ashraf <mennaalla1512@gmail.com>
2025-09-07 13:10:04 +0300

    update strc

---
+++ src/app/feature/facilities/facilities.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { ViewFacilitiesComponent } from './components/view-facilities/view-facilities.component';
+import { AddEditFacilitiesComponent } from './components/add-edit-facilities/add-edit-facilities.component';
+import { FacilitiesListComponent } from './components/facilities-list/facilities-list.component';
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'facilitiesList',pathMatch:'full'},
+  {path:'facilitiesList',component:FacilitiesListComponent},
+  {path:'add',component:AddEditFacilitiesComponent},
+  {path:'edit/:id',component:AddEditFacilitiesComponent},
+  {path:'view',component:ViewFacilitiesComponent},
+]
+@NgModule({
+  declarations: [
+    FacilitiesListComponent,
+    AddEditFacilitiesComponent,
+    ViewFacilitiesComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedModule
+  ]
+})
+export class FacilitiesModule { }
