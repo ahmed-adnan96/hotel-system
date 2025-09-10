c5e77d928c7559c499bfcab7d598e7eb24960f2b
Menna Ashraf <mennaalla1512@gmail.com>
2025-09-10 22:04:45 +0300

    add structure

---
+++ src/app/feature/ads/ads.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { ListAdsComponent } from './components/list-ads/list-ads.component';
+import { AddEditAdsComponent } from './components/add-edit-ads/add-edit-ads.component';
+
+
+const routes: Routes = [
+  {path:'',redirectTo:'adsList',pathMatch:'full'},
+  {path:'adsList',component:ListAdsComponent,title:'Ads List'},
+  {path:'addAds',component:AddEditAdsComponent,title:'Add Ads'},
+  {path:'editAds',component:AddEditAdsComponent,title:'Edit Ads'}
+]
+@NgModule({
+  declarations: [
+    AddEditAdsComponent,
+    ListAdsComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ]
+})
+export class AdsModule { }
