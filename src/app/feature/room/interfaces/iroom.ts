83fa32b022a0d3d36ed6a2c8a6531c9fc6e13da9
Menna Ashraf <mennaalla1512@gmail.com>
2025-09-10 14:45:13 +0300

    fix conflict

--- src/app/feature/room/interfaces/iroom.ts
+++ src/app/feature/room/interfaces/iroom.ts
@@ -30,34 +30,3 @@ export interface IFacility {
   _id: string;
   name: string;
 }
-
-
-export interface IRoomResponse {
-  success: boolean;
-  message: string;
-  data: Data;
-}
-
-export interface Data {
-  room: Room;
-}
-
-export interface Room {
-  roomNumber: string;
-  price: number;
-  capacity: number;
-  discount: number;
-  facilities: string[];
-  createdBy: string;
-  images: string[];
-  _id: string;
-  createdAt: string;
-  updatedAt: string;
-}
-export interface IRoomRequest {
-  roomNumber: string;
-  price: number;
-  capacity: number;
-  discount: number;
-  facilities: string[];
-}
