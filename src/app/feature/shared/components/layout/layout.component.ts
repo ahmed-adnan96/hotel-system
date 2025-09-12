75506ef3c80613291a3014ef485d81855b96c748
EmanMenisy <emenisy7@gmail.com>
2025-09-12 23:49:13 +0300

    edit side bar

--- src/app/feature/shared/components/layout/layout.component.ts
+++ src/app/feature/shared/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { AuthRoutingModule } from '../../../auth/auth-routing.module';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-layout',
@@ -9,4 +10,27 @@ import { AuthRoutingModule } from '../../../auth/auth-routing.module';
   styleUrl: './layout.component.scss',
   standalone: false,
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit {
+   screenWidth:number=0;
+   IsSideOpen:boolean=false;
+  constructor(private _SidebarService:SidebarService) {
+    
+  }
+  ngOnInit(): void {
+    this._SidebarService.isOpened$.subscribe((value)=>{this.IsSideOpen= value})
+    this._SidebarService.screenWidth$.subscribe((value)=>{this.screenWidth = value})
+  }
+ 
+
+    getBodyClass():string{
+    let style_Class=" ";
+    if(this.IsSideOpen && this.screenWidth > 768){
+           style_Class = 'when_side_open'
+    }
+    else if(this.IsSideOpen && this.screenWidth <= 768 && this.screenWidth >0){
+            style_Class = 'when_side_close'
+    }
+    return style_Class;
+
+  }
+}
